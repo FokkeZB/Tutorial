bb7cd0ec99caf5603760f1ede16f3563dc0c6093
Fokke Zandbergen <mail@fokkezb.nl>
2014-06-18 17:00:01 +0200

    Code for webview

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,10 @@
+Alloy.Collections.feed.fetch();
+
 $.index.open();
 
 function openDetail(e) {
+
+  console.debug(e);
 
   var controller = Alloy.createController('detail');
   var view = controller.getView();
