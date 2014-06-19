6c80c8091f5edc08663c5e90289d3928b2584023
Fokke Zandbergen <mail@fokkezb.nl>
2014-06-19 09:06:47 +0200

    simplify sync

---
+++ app/lib/alloy/sync/json.js
@@ -0,0 +1,36 @@
+function Sync(method, model, opts) {
+
+  if (method !== 'read') {
+    throw 'This sync adapter only reads.';
+
+  } else {
+
+    var xhr = Ti.Network.createHTTPClient({
+
+      onload: function() {
+
+        try {
+          var values = JSON.parse(this.responseText);
+
+          model.length = values.length;
+
+          opts.success((model.length === 1) ? values[0] : values, this.responseText);
+
+        } catch (e) {
+          opts.error(model, this.responseText);
+        }
+
+      },
+
+      onerror: function(e) {
+        opts.error(model, this.responseText);
+      }
+
+    });
+
+    xhr.open('GET', model.config.adapter.url);
+    xhr.send();
+  }
+}
+
+exports.sync = Sync;
