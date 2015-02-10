2b437dbd3b62ed73ea9151a97c26cde53a8de095
Fokke Zandbergen <mail@fokkezb.nl>
2015-02-10 17:25:30 +0600

    Bangladesh updates

--- app/controllers/detail.js
+++ app/controllers/detail.js
@@ -1,13 +1,11 @@
 var args = arguments[0] || {};
-
 var model = args.model;
 
 $.detail.title = model.get('title');
 
 var style = '<style>body { font-family: sans-serif; }</style>';
-
 var body = model.get('content');
 
-var htmlTemplate = _.template('<!DOCTYPE HTML><html lang="en-US"><head><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">    <meta charset="UTF-8"><title></title><%= style %></head><body><%= content %></body></html>');
+var htmlTemplate = _.template('<!DOCTYPE HTML><html lang="en-US"><head><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"><meta charset="UTF-8"><title></title><%= style %></head><body><%= content %></body></html>');
 
 $.webView.html = htmlTemplate({style:style, content:body});
